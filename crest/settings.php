ca6144d16239ae546d5fab0cb4a818a54b5c6fe3
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-06 09:09:41 +0530

    Update crest/settings crest/install

--- crest/settings.php
+++ crest/settings.php
@@ -1,7 +1,7 @@
 <?php
-define('C_REST_WEB_HOOK_URL', 'https://gicrm.ae/rest/1945/7mnw3te56u363prw/');
-// define('C_REST_CLIENT_ID', 'local.6711e822d38b97.48529652');
-// define('C_REST_CLIENT_SECRET', 'w6aszi6PgwNfx5vGLDMfBUQa7M3Ktf8MpY216nEp3qtrddtEEl');
+//define('C_REST_WEB_HOOK_URL', 'https://gicrm.ae/rest/1945/7mnw3te56u363prw/');
+define('C_REST_CLIENT_ID', 'local.67a42e7604be80.81184164');
+define('C_REST_CLIENT_SECRET', 'qNtQtuPsoke91xxeS2d45AF311Mlf5MdTHJ9zM0uQoiURPwOZt');
 
 define('DEVELOPERS_ENTITY_TYPE_ID', 1100);
 define('AGENTS_ENTITY_TYPE_ID', 1088);
