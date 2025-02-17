bff86dad58fa59a4065abaee3c864a5dba94957e
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-17 19:09:37 +0530

    Make project status required

--- views/components/add-property/specifications.php
+++ views/components/add-property/specifications.php
@@ -135,8 +135,8 @@
         </div>
         <!-- Column 3 -->
         <div class="max-w-sm">
-            <label for="project_status" class="block text-sm font-medium mb-2">Project Status</label>
-            <select id="project_status" name="project_status" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+            <label for="project_status" class="block text-sm font-medium mb-2">Project Status <span class="text-danger">*</span></label>
+            <select id="project_status" name="project_status" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required>
                 <option value="">Please select</option>
                 <option value="off_plan">Off Plan</option>
                 <option value="off_plan_primary">Off-Plan Primary</option>
