d94ae1a63587a68796d302a115cce0b0125f10ab
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-20 09:47:36 +0530

    Update back buttons

--- views/properties/add.php
+++ views/properties/add.php
@@ -30,7 +30,7 @@
             <?php include_once('views/components/add-property/status.php'); ?>
 
             <div class="mt-6 flex justify-end space-x-4">
-                <button type="button" onclick="window.location.href = 'index.php?page=properties'" class="px-6 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1">
+                <button type="button" onclick="javascript:history.back()" class="px-6 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1">
                     Back
                 </button>
                 <button type="submit" id="submitButton" class="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1">
